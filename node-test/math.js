9f2dc3e1f93efb6aa8c79a6f9f0590bd9ab4cae1
kjb990202 <kjb990202@gmail.com>
2023-10-10 18:13:33 +0900

    study day8 : node.js

---
+++ node-test/math.js
@@ -0,0 +1,21 @@
+const add = (a,b) => a + b;
+const minus = (a,b) => a - b;
+const PI = 3.14;
+
+module.exports = {
+    add,
+    minus,
+    PI
+};
+
+// const data = {
+//     add : add,
+//     minus : minus,
+//     PI : PI
+// };
+
+
+
+// const add2 = (a,b) => {
+//     return a + b;
+// } 
