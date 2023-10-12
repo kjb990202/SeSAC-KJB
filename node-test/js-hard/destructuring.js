107c090d2a72b92b89a9b54dc0345a9f4bfcffb4
kjb990202 <kjb990202@gmail.com>
2023-10-12 18:11:20 +0900

    study day9 : node.js

---
+++ node-test/js-hard/destructuring.js
@@ -0,0 +1,60 @@
+const fruits = ["apple", "banana", "grape"];
+
+const [apple2, banana2 = "banana2", grape2, straw = "straw" ] = fruits;
+console.log(apple2, banana2);
+
+// 사실상 아래 코드와 동일한 작업을 하고 있음
+// const apple2 = fruits[0]
+// const banana2 = fruits[1]
+// const grape2 = fruits[2]
+
+let x = 1,
+y = 3;
+// x = 3, y = 1
+[x, y] = [y, x];
+
+console.log(x, y);
+
+// let temp = x; // temp = 1
+// x = y // x = 3, y = 3, temp
+// // y = x // y = 3
+// y = temp;
+
+const obj = {
+    name : "lily",
+    gender : "여",
+    age : 99,
+};
+
+// const { age, name, test = "test" } = obj;
+// console.log(age, test);
+
+// key 이름 바꾸는 방법
+const { age, name : name2 } = obj;
+console.log(age, name2);
+
+// const age = obj.age;
+// const name = obj.name;
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = ["a", "b", "c", "d", "e"];
+
+// [1, 2, 3, 4, 5, "a", "b", "c", "d", "e"]
+
+// const arr3 = [arr1[0]~~~~~, arr2[0], arr2[1], ~~~~~]
+// for()
+
+const arr3 = [...arr1, ...arr2];
+console.log("arr3", arr3 )
+
+// ...[ 1, 2, 3, 4, 5] => 1, 2, 3, 4, 5
+const hello = [..."hello"]
+// [ "h", "e", "l", "l", "o"]
+console.log("hello", hello);
+"hello".split("")
+
+const word1 = "abc";
+const word2 = "xyz";
+
+const word = [...word1, ...word2];
+console.log(word);
