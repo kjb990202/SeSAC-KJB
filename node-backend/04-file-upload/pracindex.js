c8711afe2433de7d9cdce1a8f982271190717b56
kjb990202 <kjb990202@gmail.com>
2023-10-21 16:53:14 +0900

    파일 업로드 실습

--- node-backend/04-file-upload/pracindex.js
+++ node-backend/04-file-upload/pracindex.js
@@ -1,24 +1,11 @@
 const express = require("express");
 const multer = require("multer"); // multer 불러오기
 const path = require("path");
-// path: 파일 경로를 받았을 때, 그 조작을 도와준다.
-// ex) 확장자 추출. 파일이름을 추출
-console.log("hi.txt의 확장자:", path.extname("hi.txt"));
-console.log("hi.txt의 이름:", path.basename("hi.txt", path.extname("hi.txt")));
 
 const app = express();
 const PORT = 8000;
 
-// 클라이언트가 uploads 폴더에 저장한 이미지파일에 접근할 수 있도록, 미들웨어 작성
 app.use("/uploads", express.static(__dirname + "/uploads"));
-
-// upload라는 객체 안에는 미들웨어 함수가 존재. single(), array()
-// 미들웨어 : 요청과 응답 사이에 존재.
-// multer가 임의의 문자열을 생성해서 그 문자열을 파일 이름으로 만듦.
-// 심지어 확장자도 붙여주지 않음.
-const upload = multer({
-  dest: "uploads/",
-});
 
 const uploadDetail = multer({
   storage: multer.diskStorage({
@@ -26,25 +13,16 @@ const uploadDetail = multer({
       done(null, "uploads/");
     },
     filename: function (req, file, done) {
-      console.log(file); // file.originalname : 미니언.webp
-      const ext = path.extname(file.originalname); // .webp
-      const basename = path.basename(file.originalname, ext); // 미니언
-      const fileName = basename + "_" + Date.now() + ext; // 미니언_123453156.webp
+      
+      const ext = path.extname(file.originalname); 
+      const basename = path.basename(file.originalname, ext); 
+      const fileName = basename + "_" + Date.now() + ext; 
 
       done(null, fileName);
     },
   }),
   limits: { fileSize: 5 * 1024 * 1024 }, // 5MB 제한
 });
-
-// uploads/미니언_123453156.webp
-
-// storage : 파일을 저장할 정보
-// -- diskStorage : 파일을 디스크에 저장하기 위한 기능을 제공하는 메소드
-// ----- destination : 파일이 저장될 경로
-// ----- filename : 파일이 저장될 이름
-// limits
-// -- fileSize : 파일의 최대 크기
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -89,7 +67,7 @@ app.post("/upload/array", uploadDetail.array("userfile"), function (req, res) {
 // fields() : 파일 여러개 업로드. name이 두 개 이상(input이 2개 이상).
 // req.files 생성
 app.post(
-  "/upload/fields",
+  "/uploads/fields",
   uploadDetail.fields([{ name: "userfile1" }, { name: "userfile2" }]),
   function (req, res) {
     console.log("file 여러개(ver2):", req.files);
